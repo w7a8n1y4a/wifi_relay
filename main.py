6a27e4f620e45dd1ad71d53a3ebae60a9bb6e7eb
Ivan Serebrennikov <admin@silberworks.com>
2023-09-23 21:08:35 +0300

    feat(aes): add pad func

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from ucryptolib import aes
 from config import settings
 import uos
 
-key = b'\xe1\xac<p\xaa.\x15\x03\x16\xa9\x1fq\x98Lw:\xa3;\xdb\x04\xb6\x8d*\xc3\x8e\xd5(\xfd\x17\xcbP$'
-iv = b'!\xb2>\\^\xb0Q\xf7\x87\x93\x02\xc2\x8b\xc3\xb5y'
+def pad(m):
+    return m+chr(16-len(m)%16)*(16-len(m)%16)
+
+key = b'\xfb\xd0%T\xa8bn*:\xf0\xed\xe6\xb8}\xe5e\x86{#\xe80\xf7\xb4\xc1\x03/9]NKf\xcd'
+iv = b'\xb4f\xd6~\x1eA\xfca\xcf\x93\xca\xe0z;\x88\r'
 
 print(key, iv)
 
 MODE_CBC = 2
 cipher = aes(key, MODE_CBC, iv)
 
-plain = "Text and text wtf. w7a8n1y4a"
-print(plain)
-
-padded = plain + " " * (16 - len(plain) % 16)
-print(len(padded))
-encrypted = cipher.encrypt(padded)
+plain = "Text and text wtf. w7a8n1y4a Text and text wtf. w7a8n1y4aText and text wtf. w7a8n1y4a"
+print(len(plain))
+plain = pad(plain)
+encrypted = cipher.encrypt(plain)
 print(f"{str(encrypted)}")
   
 decipher = aes(key, MODE_CBC, iv)
