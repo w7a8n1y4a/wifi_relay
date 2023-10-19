dca2ab4e8b1ed0e1501a018a686b80e6f0752fb2
Ivan Serebrennikov <admin@silberworks.com>
2023-10-19 23:44:02 +0300

    feat(mqtt): add co2 sensor with pwm

--- boot.py
+++ boot.py
@@ -6,6 +6,7 @@ sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
     sta_if.active(True)
     sta_if.connect(settings.WIFI_SSID, settings.WIFI_PASS)
+    print(settings.WIFI_SSID, settings.WIFI_PASS)
     while not sta_if.isconnected():
         pass
 
--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from utils.aes import aes_encode, aes_decode
+import time
+import uos
+import machine
+import ubinascii
+from umqtt.simple import MQTTClient
 
 from config import settings
 
-text = "One two three four five six seven aht neun zein elf zwoelf"
+# Default MQTT MQTT_BROKER to connect to
+SERVER = settings.PEPEUNIT_URL
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+TOPIC = b"co2"
 
-print(text)
-cypher = aes_encode(text)
-print(cypher)
-decrypt = aes_decode(cypher)
-print(decrypt)
+
+def reset():
+    print("Resetting...")
+    time.sleep(5)
+    machine.reset()
+    
+
+def main():
+    mqttClient = MQTTClient(CLIENT_ID, SERVER, keepalive=60)
+    mqttClient.connect()
+    print(f"Connected to MQTT  Broker :: {SERVER}")
+
+    while True:
+        time_true_pulse_us = machine.time_pulse_us(machine.Pin(15, machine.Pin.IN), 1000000)
+        print(time_true_pulse_us)
+        ppm = 2000 * (time_true_pulse_us/1000000)
+        random_temp = f'{time.ticks_ms()} - ppm - {str(ppm)}'
+        print(f"millis - {time.ticks_ms()} - ppm - {str(ppm)}")
+        mqttClient.publish(TOPIC, str(random_temp).encode())
+        time.sleep(5.123)
+    mqttClient.disconnect()
+    
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except OSError as e:
+        print("Error: " + str(e))
+        reset()
